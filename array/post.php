5805f01a34ddae5646f67315ca8184532f6c2451
Anderson Lemos <andersonlemos1914@gmail.com>
2025-01-22 20:33:12 -0300

    adicionando continuação aula array

--- array/post.php
+++ array/post.php
@@ -1,3 +1,22 @@
 <div class="titulo">$_POST</div>
 
+
+<form action="" method="post">
+    <input type="text" name="name">
+    <input type="text" name="sobrenome">
+    <select name="estado">
+        <option value="FOR">Fortaleza</option>
+        <option value="CAU">Caucaia</option>
+    </select>
+    <button>Enviar</button>
+</form>
+
+<style>
+    form > * {
+        font-size: 1.8rem;
+    }
+</style>
 <?php
+print_r($_GET);
+echo '<br>';
+print_r($_POST);
